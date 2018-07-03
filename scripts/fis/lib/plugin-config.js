04799d20c7e71865f57de96eef7d6e9572415b7b
fisker <lionkay@gmail.com>
2018-07-03 11:15:52 +0800

    rename uglifyjs

--- scripts/fis/lib/plugin-config.js
+++ scripts/fis/lib/plugin-config.js
@@ -70,7 +70,7 @@ function getPluginConfig() {
       remove: false,
       browsers: browserslist
     },
-    'fis3-optimizer-uglify-js-latest': uglifyJSConfig,
+    'fis3-optimizer-uglifyjs-latest': uglifyJSConfig,
     'fis-optimizer-clean-css-2x': {
       advanced: false,
       aggressiveMerging: false,
