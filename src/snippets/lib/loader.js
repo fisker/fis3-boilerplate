b9dac9aa96887870944f7e5c1af8acfe8e595774
fisker <lionkay@gmail.com>
2018-08-08 19:18:45 +0800

    auto version

--- src/snippets/lib/loader.js
+++ src/snippets/lib/loader.js
@@ -10,20 +10,24 @@ const {
   createLink,
 } = require('./common.js')
 
-function loadNodeModules(mod) {
-  console.log(require.resolve(mod))
+const path = require('path')
+function getPackageInfo(pkgName) {
+  let arr = require.resolve(pkgName).replace(/\\/g, '/').split('/')
+  let dir = arr.slice(0, arr.lastIndexOf('node_modules') + 2).join('/')
+  return require(path.join(path.normalize(dir), 'package.json'))
 }
 
 
 const loader = {
-  dd_belatedpng: function() {
+  dd_belatedpng() {
     if (project.device != 'mobile' && project.legacyIe <= 6) {
-      const file = env.production ? 'DD_belatedPNG_0.0.8a.min.js' : 'DD_belatedPNG_0.0.8a.js'
-      const src = `/assets/vendors/dd_belatedpng/0.0.8a/dist/${file}`
+      const version = getPackageInfo('dd_belatedpng').version
+      const file = env.production ? 'DD_belatedPNG_${version}.min.js' : 'DD_belatedPNG_${version}.js'
+      const src = `/assets/vendors/dd_belatedpng/${version}/dist/${file}`
       return `<!--[if IE 6]>${createScript(src)}<script>DD_belatedPNG.fix('*')</script><![endif]-->`
     }
   },
-  jquery: function() {
+  jquery() {
     const file = env.production ? 'jquery.min.js' : 'jquery.js'
 
     function jquery(version) {
@@ -41,18 +45,45 @@ const loader = {
 
     return html.join('\n')
   },
-  html5shiv: function() {
+  html5shiv() {
     if (project.device != 'mobile' && project.legacyIe < 9) {
+      const version = getPackageInfo('html5shiv').version
       const file = env.production ? 'html5shiv.min.js' : 'html5shiv.js'
-      const src = `/assets/vendors/html5shiv/3.7.3-pre/dist/${file}`
+      const src = `/assets/vendors/html5shiv/${version}/dist/${file}`
       return `<!--[if lt IE 9]>${createScript(src)}<![endif]-->`
     }
   },
-  rem: function() {
+  rem() {
     if (project.flexibleRem) {
       return createScript('/assets/scripts/component/_rem.js?__inline')
     }
   },
+  vue() {
+    const file = env.production ? 'vue.min.js' : 'vue.js'
+    const version = getPackageInfo('vue').version
+
+    return createScript(`/assets/vendors/vue/${version}/dist/${file}`)
+  },
+  'element-ui': function() {
+    const version = getPackageInfo('element-ui').version
+    return createScript(`/assets/vendors/element-ui/${version}/lib/index.js`)
+  },
+  'element-ui.css': function() {
+    const version = getPackageInfo('element-ui').version
+    return createLink(`/assets/vendors/element-ui/${version}/lib/theme-chalk/index.css`)
+  },
+  dom4() {
+    const version = getPackageInfo('dom4').version
+    return createScript(`/assets/vendors/dom4/${version}/build/dom4.js`)
+  },
+  'core-js': () => {
+    const version = getPackageInfo('core-js').version
+    return createScript(`/assets/vendors/core-js/${version}/client/shim.min.js`)
+  },
+  nprogress() {
+    const version = getPackageInfo('nprogress').version
+    return createScript(`/assets/vendors/nprogress/${version}/nprogress.min.js`)
+  }
 }
 
 module.exports = loader
