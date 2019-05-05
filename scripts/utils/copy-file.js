120d766a6ba90c197d76145dd77858cdb7361f46
fisker <lionkay@gmail.com>
2019-05-05 15:36:37 +0800

    chore: mkdirp for node 8

--- scripts/utils/copy-file.js
+++ scripts/utils/copy-file.js
@@ -1,8 +1,36 @@
 const fs = require('fs')
 const path = require('path')
 
+function mkdir(dir, options) {
+  try {
+    const stat = fs.statSync(dir)
+
+    if (stat.isDirectory()) {
+      return true
+    }
+  } catch (_) {}
+
+  try {
+    fs.mkdirSync(dir, options)
+  } catch (_) {}
+}
+
+function mkdirP(dir, options) {
+  try {
+    return fs.mkdirSync(dir, options)
+  } catch (_) {}
+
+  const arr = path.normalize(dir).split(path.sep)
+  let current = arr.shift()
+
+  for (const seg of arr) {
+    current = path.join(current, seg)
+    mkdir(current, options)
+  }
+}
+
 function copyFile(source, target) {
-  fs.mkdirSync(path.dirname(target), {
+  mkdirP(path.dirname(target), {
     recursive: true,
   })
 
